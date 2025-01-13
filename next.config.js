c6e938b547a178b2924e74db04f8756cd8a521c9
ci-psy <31685851+ci-psy@users.noreply.github.com>
2025-01-13 09:13:06 -0800

    Update

--- next.config.js
+++ next.config.js
@@ -4,12 +4,21 @@ const nextConfig = {
   trailingSlash: true,
   transpilePackages: ["geist"],
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_CUSTOM_DOMAIN ? '' : '/Norn-site',
-  assetPrefix: process.env.NEXT_PUBLIC_CUSTOM_DOMAIN ? '' : '/Norn-site',
+  basePath: process.env.NODE_ENV === 'production' 
+    ? (process.env.NEXT_PUBLIC_CUSTOM_DOMAIN ? '' : '/Norn-site')
+    : '',
+  assetPrefix: process.env.NODE_ENV === 'production'
+    ? (process.env.NEXT_PUBLIC_CUSTOM_DOMAIN ? '' : '/Norn-site')
+    : '',
   images: {
     unoptimized: true,
-    path: process.env.NEXT_PUBLIC_CUSTOM_DOMAIN ? '/_next/image' : '/Norn-site/_next/image',
+    path: process.env.NODE_ENV === 'production'
+      ? (process.env.NEXT_PUBLIC_CUSTOM_DOMAIN ? '/_next/image' : '/Norn-site/_next/image')
+      : '/_next/image',
   },
+  experimental: {
+    appDir: true
+  }
 }
 
 module.exports = nextConfig
