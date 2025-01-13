ad587de5fca598014263c713134f5fe29bd3211f
ci-psy <31685851+ci-psy@users.noreply.github.com>
2025-01-13 09:34:39 -0800

    Update

--- next.config.js
+++ next.config.js
@@ -15,9 +15,6 @@ const nextConfig = {
     path: process.env.NODE_ENV === 'production'
       ? (process.env.NEXT_PUBLIC_CUSTOM_DOMAIN ? '/_next/image' : '/Norn-site/_next/image')
       : '/_next/image',
-  },
-  experimental: {
-    appDir: true
   }
 }
 
