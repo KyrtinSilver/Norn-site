fb3c624fea5aca3b4f52f21e6bc5fe4b6a04f910
ci-psy <31685851+ci-psy@users.noreply.github.com>
2025-02-01 12:23:13 -0800

    Draft

--- src/app/(marketing)/collaboration/community/page.tsx
+++ src/app/(marketing)/collaboration/community/page.tsx
@@ -26,7 +26,7 @@ export default function CommunityPage() {
             </p>
             <Button asChild size="lg">
               <Link href="/collaboration">
-                Back to the Future
+                Back to Collaboration
               </Link>
             </Button>
           </div>
